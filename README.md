c6e415a7d683baaf3cf27e1c94ccabe61c84bdb1
salah <srachdi@student.1337.ma>
2025-02-01 17:29:02 +0100

    machine setup script done

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Inception of things
+
+## Resources
+- [K8s Docs](https://kubernetes.io/docs/home/)
+- [K3s Docs](https://docs.k3s.io/)
+- [ArgoCD on K3d](https://dev.to/danielcristho/k3d-getting-started-with-argocd-5c6l)
+- [GitOps on a Laptop with K3D and ArgoCD](https://www.sokube.io/en/blog/gitops-on-a-laptop-with-k3d-and-argocd-en)
+- [K3d Docs](https://k3d.io/)
+- [ArgoCD Docs](https://argo-cd.readthedocs.io/en/stable/)
---
+++ p3/scripts/setup_k3d.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+# Update package list and install dependencies silently
+echo "Installing dependencies..."
+apt-get update -q > /dev/null
+apt-get install -y -q curl vim > /dev/null
+# Install Docker
+echo "Installing Docker..."
+curl -fsSL https://get.docker.com -o get-docker.sh > /dev/null
+sh get-docker.sh > /dev/null
+
+
+# Install k3d
+echo "Installing k3d..."
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash > /dev/null
+
+# Create a k3d cluster
+echo "Creating  'dev-cluster'..."
+k3d cluster create dev-cluster --api-port 6550 --port 8081:80@loadbalancer --port 8082:443@loadbalancer > /dev/null
+
+# Install kubectl
+echo "Installing kubectl..."
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" > /dev/null
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl > /dev/null
+
+export KUBECONFIG="$(k3d kubeconfig write dev-cluster)" > /dev/null
+# Check k3d cluster
+echo "Checking k3d cluster..."
+kubectl cluster-info
+kubectl get nodes
